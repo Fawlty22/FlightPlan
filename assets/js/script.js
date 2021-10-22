034266e7f1194aace69a4f6b46b3c609daf74788
Eric <ericyoung111@gmail.com>
2021-10-21 19:34:13 -0600

    feature/api-calls the currency conversion api call works. Flight data does not.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,29 @@
 //second money card appears
 
 //list of flights for destinations
+//documentation for flight API https://www.flightapi.io/docs/#getting-started
+const flightAPIKey = "6170dd58559f311752870242"
+const exchangeAPIKey = "10a0a9e87b4e3dfb6a11dfe5"
 
+//API call for flight data
+fetch(`https://api.flightapi.io/roundtrip/${flightAPIKey}/LHR/LAX/2021-10-25/2021-10-29/2/0/1/Economy/USD`)
+.then(function(response){
+    return response.json();
+})
+.then(function(flightData){
+    console.log(flightData)
+})
+
+//API call for currency conversion
+fetch(`https://v6.exchangerate-api.com/v6/${exchangeAPIKey}/latest/USD`)
+.then(function(response){
+    return response.json();
+})
+.then(function(exchangeRates){
+    console.log(exchangeRates);
+});
+
+//doesn't work with the catch. Need to fix.     
+// .catch(function(error) {
+// 	console.log(error);
+// });
