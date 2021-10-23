d5d81e1e2477c4258a516d76daa5e7acc4eb59b6
Eric <ericyoung111@gmail.com>
2021-10-22 19:56:36 -0600

    found a second API that pulls quotes for flights

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,20 +9,26 @@ var displayLocationCards = function() {
 }
 
 //list of flights for destinations
-//documentation for flight API https://www.flightapi.io/docs/#getting-started
-const flightAPIKey = "6170dd58559f311752870242"
-const exchangeAPIKey = "10a0a9e87b4e3dfb6a11dfe5"
 
 //API call for flight data
-fetch(`https://api.flightapi.io/roundtrip/${flightAPIKey}/LHR/LAX/2021-10-25/2021-10-29/2/0/1/Economy/USD`)
-.then(function(response){
-    return response.json();
+
+fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2021-10-30?inboundpartialdate=2021-11-10", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+		"x-rapidapi-key": "66c69d12e4msh8b9325bed3418c9p1018dbjsn8cf4ae03c51f"
+	}
 })
-.then(function(flightData){
-    console.log(flightData)
+.then(response => {
+	return response.json();
+})
+.then(function(response){
+    console.log(response);
 })
 
+
 //API call for currency conversion
+const exchangeAPIKey = "10a0a9e87b4e3dfb6a11dfe5"
 fetch(`https://v6.exchangerate-api.com/v6/${exchangeAPIKey}/latest/USD`)
 .then(function(response){
     return response.json();
@@ -35,12 +41,6 @@ fetch(`https://v6.exchangerate-api.com/v6/${exchangeAPIKey}/latest/USD`)
 // .catch(function(error) {
 // 	console.log(error);
 // });
-
-
-
-
-
-
 
 $('#location-input').on('click', 'button', function(){
     displayBudgetCard();
